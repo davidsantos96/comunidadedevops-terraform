250bb66c4f9f75bc6adb3aea72b8eed4c46366cd
David Patricio <davidsantos@ext.cvccorp.com.br>
2025-05-28 14:52:20 -0300

    implementação na vpc e provider

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta2"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadedevops-bucket-david"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
